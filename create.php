0af2e83cbd05df22fe39abdc9911dcfcd55e5c75
zeng.zi.hao <zeng.zi.hao.work@gmail.com>
2022-08-11 18:22:07 +0800

    優化code

--- create.php
+++ create.php
@@ -7,29 +7,32 @@
 
     // 驗證規則
     $phone_isnum = "/^[0-9]*$/";
-    $name_isword = "/^[\u4e00-\u9fa5_a-zA-Z0-9]+$/";
+    $name_isword = "/^[\w\x\u4e00-\u9fa5_a-zA-Z0-9]+$/u";
+    // $name_isword = "/[\w\x{4e00}-\x{9fa5}]+/u";
     
     // 取得post資料
     $name = $request -> name;
     $phone = $request -> phone;
     $remark = $request -> remark;
     
-    // 將資料進行編碼、消除前後空白
+    // addQ將單引號配上\
+    // 消除前後空白
     $name = $conn -> addQ(trim($name));
     $phone = $conn -> addQ(trim($phone));
     $remark = $conn -> addQ(trim($remark));
 
     // 切割
-    $name_arr = str_split($name);
-    $phone_arr = str_split($phone);
-    $remark_arr = str_split($remark);
+    $name_arr = mb_str_split($name);
+    $phone_arr = mb_str_split($phone);
+    $remark_arr = mb_str_split($remark);
 
     // 判斷create規則
+    // 不能空值、姓名小於5、電話小於10、只能中英文和數字
     if($name_arr[0] == "" || $phone_arr[0] == ""){
     }
-    if(count($name_arr) > 5 || count($phone_arr) > 10){
+    else if(count($name_arr) > 5 || count($phone_arr) > 10){
     }
-    elseif(preg_match($name_isword,$name) == false || preg_match($phone_isnum,$phone)  == false){  
+    else if(preg_match($name_isword,$name) == false || preg_match($phone_isnum,$phone)  == false){  
     }
     else{
         $conn -> execute(" INSERT INTO users VALUES('', '$name', '$phone', '$remark')");
